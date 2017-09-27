733cc3b76356be0cb42bbcdf69430d0603b0d13f
Kris Jenkins <krisajenkins@gmail.com>
2017-09-27 20:24:54 +0100

    Roughly working!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
@@ -5,7 +6,7 @@ module Lib
   ( someFunc
   ) where
 
-import           Control.Lens (makeLenses, preview, _Just)
+import           Control.Lens (Getting, Lens', makeLenses, preview, view, _Just)
 import           Data.List    (sortOn)
 import           Data.Maybe   (fromMaybe)
 import           Data.Set     (Set)
@@ -16,9 +17,9 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data Point = Point
-  { _x    :: Int
-  , _y    :: Int
-  , _z    :: Int
+  { _px   :: Int
+  , _py   :: Int
+  , _pz   :: Int
   , _char :: Char
   } deriving (Eq, Show, Ord)
 
@@ -41,21 +42,37 @@ aWorld =
     , Point 1 1 3 'z'
     ]
 
-elevationChar :: World -> (Int, Int) -> Char
-elevationChar world (x, y) = do
+elevationChar
+  :: (Ord a, Eq a)
+  => World
+  -> Getting a Point a
+  -> (Getting a Point a, Getting a Point a)
+  -> (a, a)
+  -> Char
+elevationChar world sortLens (axis1Lens, axis2Lens) (axis1Value, axis2Value) = do
   fromMaybe ' ' $
     preview (_Just . char) $
     headMay $
-    sortOn _z $
-    filter (\(Point a b c l) -> (a == x) && (b == y)) $ Set.toList world
+    sortOn (view sortLens) $
+    filter
+      (\point ->
+         (view axis1Lens point == axis1Value) &&
+         (view axis2Lens point == axis2Value)) $
+    Set.toList world
 
-elevation :: IO ()
-elevation = do
-  putStrLn "Elevation"
-  let yS = reverse $ [0 .. 10]
-  mapM_ printRow yS
+renderView title sortLens (axis1Lens, axis2Lens) (axis1Range, axis2Range) = do
+  putStrLn title
+  mapM_ printRow axis2Range
   where
-    printRow y = do
-      let xS = [0 .. 10]
-          chars = fmap (\x -> elevationChar aWorld (x, y)) xS
-      putStrLn chars
+    printRow y =
+      putStrLn $
+      fmap
+        (\x -> elevationChar aWorld sortLens (axis1Lens, axis2Lens) (x, y))
+        axis1Range
+
+main = do
+  renderView "Plan" py (px, pz) ([0 .. 10], reverse [0 .. 10])
+  putStrLn "-----"
+  renderView "Elevation" pz (px, py) ([0 .. 10], reverse [0 .. 10])
+  putStrLn "-----"
+  renderView "Section" px (pz, py) (reverse [0 .. 10], reverse [0 .. 10])
