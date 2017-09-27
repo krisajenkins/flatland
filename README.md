fef29ce0f63b9a28e4563b26cad76371c4d3f661
Kris Jenkins <krisajenkins@gmail.com>
2017-09-27 23:59:08 +0100

    Final version.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# flatland
+# A 3D projection system (CAD-like) in Haskell
+
+Written for September 2017's [West London Hack Night](http://www.meetup.com/West-London-Hack-Night/).
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,19 +7,17 @@ module Lib
   ) where
 
 import           Control.Lens                 (Getting, Lens', makeLenses,
-                                               preview, view, _Just)
+                                               preview, to, view, _Just)
 import           Data.List                    (sortOn)
 import           Data.Maybe                   (fromMaybe)
 import           Data.Monoid                  ((<>))
+import           Data.Ord                     (Down (Down))
 import           Data.Set                     (Set)
 import qualified Data.Set                     as Set
 import           Safe                         (headMay)
 import           Text.PrettyPrint.ANSI.Leijen (Doc, blue, dullred, green,
                                                putDoc, red, string, white,
                                                yellow)
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
 
 data Color
   = Red
@@ -38,56 +36,121 @@ data Point = Point
 
 makeLenses ''Point
 
+docColor :: Color -> Doc -> Doc
+docColor White = white
+docColor Red   = red
+docColor Green = green
+docColor Blue  = blue
+
 type World = Set Point
 
 aWorld :: World
-aWorld = Set.fromList (w <> l <> h)
+aWorld =
+  Set.fromList $
+  frontDetails <> frontFace <> backFace <> (deepStrut 1 1) <> (deepStrut 1 8) <>
+  chimney <>
+  (deepStrut 8 1) <>
+  (deepStrut 8 8) <>
+  (deepStrut 4 11) <>
+  (deepStrut 5 11)
   where
-    w =
-      [ Point 1 5 2 'W' Red
-      , Point 1 5 8 'W' Red
-      , Point 1 4 2 'W' Red
-      , Point 1 4 8 'W' Red
-      , Point 1 3 2 'W' Red
-      , Point 1 3 8 'W' Red
-      , Point 1 3 5 'W' Red
-      , Point 1 2 2 'W' Red
-      , Point 1 2 8 'W' Red
-      , Point 1 2 5 'W' Red
-      , Point 1 1 3 'W' Red
-      , Point 1 1 4 'W' Red
-      , Point 1 1 6 'W' Red
-      , Point 1 1 7 'W' Red
+    backFace =
+      [ Point 1 1 14 '#' Blue
+      , Point 1 2 14 '#' Blue
+      , Point 1 3 14 '#' Blue
+      , Point 1 4 14 '#' Blue
+      , Point 1 5 14 '#' Blue
+      , Point 1 6 14 '#' Blue
+      , Point 1 7 14 '#' Blue
+      , Point 1 8 14 '#' Blue
+      , Point 8 1 14 '#' Blue
+      , Point 8 2 14 '#' Blue
+      , Point 8 3 14 '#' Blue
+      , Point 8 4 14 '#' Blue
+      , Point 8 5 14 '#' Blue
+      , Point 8 6 14 '#' Blue
+      , Point 8 7 14 '#' Blue
+      , Point 8 8 14 '#' Blue
+      , Point 2 1 14 '#' Blue
+      , Point 3 1 14 '#' Blue
+      , Point 4 1 14 '#' Blue
+      , Point 5 1 14 '#' Blue
+      , Point 6 1 14 '#' Blue
+      , Point 7 1 14 '#' Blue
+      , Point 8 1 14 '#' Blue
+      , Point 1 8 14 '#' Blue
+      , Point 2 9 14 '#' Blue
+      , Point 3 10 14 '#' Blue
+      , Point 4 11 14 '#' Blue
+      , Point 5 11 14 '#' Blue
+      , Point 6 10 14 '#' Blue
+      , Point 7 9 14 '#' Blue
+      , Point 8 8 14 '#' Blue
       ]
-    l =
-      [ Point 2 5 1 'L' Blue
-      , Point 2 4 1 'L' Blue
-      , Point 2 3 1 'L' Blue
-      , Point 2 2 1 'L' Blue
-      , Point 2 1 1 'L' Blue
-      , Point 3 1 1 'L' Blue
-      , Point 4 1 1 'L' Blue
-      , Point 5 1 1 'L' Blue
-      , Point 6 1 1 'L' Blue
+    deepStrut x y =
+      [ Point x y 2 '=' Blue
+      , Point x y 3 '=' Blue
+      , Point x y 4 '=' Blue
+      , Point x y 5 '=' Blue
+      , Point x y 6 '=' Blue
+      , Point x y 7 '=' Blue
+      , Point x y 8 '=' Blue
+      , Point x y 9 '=' Blue
+      , Point x y 10 '=' Blue
+      , Point x y 11 '=' Blue
+      , Point x y 12 '=' Blue
+      , Point x y 13 '=' Blue
       ]
-    h =
-      [ Point 2 1 8 'H' Green
-      , Point 6 1 8 'H' Green
-      , Point 2 1 7 'H' Green
-      , Point 6 1 7 'H' Green
-      , Point 2 1 6 'H' Green
-      , Point 6 1 6 'H' Green
-      , Point 2 1 5 'H' Green
-      , Point 3 1 5 'H' Green
-      , Point 4 1 5 'H' Green
-      , Point 5 1 5 'H' Green
-      , Point 6 1 5 'H' Green
-      , Point 2 1 4 'H' Green
-      , Point 6 1 4 'H' Green
-      , Point 2 1 3 'H' Green
-      , Point 6 1 3 'H' Green
-      , Point 2 1 2 'H' Green
-      , Point 6 1 2 'H' Green
+    chimney =
+      [Point 2 9 6 '^' White, Point 2 10 6 '^' White, Point 2 11 6 '^' White]
+    frontFace =
+      [ Point 1 1 1 '#' Blue
+      , Point 1 2 1 '#' Blue
+      , Point 1 3 1 '#' Blue
+      , Point 1 4 1 '#' Blue
+      , Point 1 5 1 '#' Blue
+      , Point 1 6 1 '#' Blue
+      , Point 1 7 1 '#' Blue
+      , Point 1 8 1 '#' Blue
+      , Point 8 1 1 '#' Blue
+      , Point 8 2 1 '#' Blue
+      , Point 8 3 1 '#' Blue
+      , Point 8 4 1 '#' Blue
+      , Point 8 5 1 '#' Blue
+      , Point 8 6 1 '#' Blue
+      , Point 8 7 1 '#' Blue
+      , Point 8 8 1 '#' Blue
+      , Point 2 1 1 '#' Blue
+      , Point 3 1 1 '#' Blue
+      , Point 4 1 1 '#' Blue
+      , Point 5 1 1 '#' Blue
+      , Point 6 1 1 '#' Blue
+      , Point 7 1 1 '#' Blue
+      , Point 8 1 1 '#' Blue
+      , Point 1 8 1 '#' Blue
+      , Point 2 9 1 '#' Blue
+      , Point 3 10 1 '#' Blue
+      , Point 4 11 1 '#' Blue
+      , Point 5 11 1 '#' Blue
+      , Point 6 10 1 '#' Blue
+      , Point 7 9 1 '#' Blue
+      , Point 8 8 1 '#' Blue
+      ]
+    frontDetails =
+      [ Point 3 7 1 '+' Red
+      , Point 6 7 1 '+' Red
+      , Point 4 1 0 '|' Green
+      , Point 5 1 0 '|' Green
+      , Point 4 2 0 '|' Green
+      , Point 5 2 0 '|' Green
+      , Point 4 3 0 '_' Green
+      , Point 5 3 0 '_' Green
+      , Point 4 1 (-1) '|' Green
+      , Point 5 1 (-1) '|' Green
+      , Point 4 2 (-1) '|' Green
+      , Point 5 2 (-1) '|' Green
+      , Point 4 3 (-1) '_' Green
+      , Point 5 3 (-1) '_' Green
       ]
 
 renderPoint
@@ -111,12 +174,6 @@ renderPoint world sortLens (axis1Lens, axis2Lens) (axis1Value, axis2Value) =
            (view axis2Lens point == axis2Value)) $
       Set.toList world
 
-docColor :: Color -> Doc -> Doc
-docColor White = white
-docColor Red   = red
-docColor Green = green
-docColor Blue  = blue
-
 renderView title sortLens (axis1Lens, axis2Lens) (axis1Range, axis2Range) = do
   putDoc $ yellow $ string title
   putStrLn ""
@@ -124,15 +181,18 @@ renderView title sortLens (axis1Lens, axis2Lens) (axis1Range, axis2Range) = do
   where
     printRow y = do
       let docs :: [Doc] =
-            fmap
-              (\x -> renderPoint aWorld sortLens (axis1Lens, axis2Lens) (x, y))
-              axis1Range
-      putDoc ((mconcat docs))
+            (\x -> renderPoint aWorld sortLens (axis1Lens, axis2Lens) (x, y)) <$>
+            axis1Range
+      putDoc (mconcat docs)
       putStrLn ""
 
 render = do
-  renderView "Plan" py (px, pz) ([0 .. 10], reverse [0 .. 10])
+  renderView
+    "Plan"
+    (py . to negate)
+    (px, pz)
+    ([(-2) .. 15], reverse [(-2) .. 15])
   putStrLn "-----"
-  renderView "Elevation" pz (px, py) ([0 .. 10], reverse [0 .. 10])
+  renderView "Elevation" pz (px, py) ([(-2) .. 15], reverse [(-2) .. 15])
   putStrLn "-----"
-  renderView "Section" px (pz, py) (reverse [0 .. 10], reverse [0 .. 10])
+  renderView "Section" px (pz, py) (reverse [(-2) .. 15], reverse [(-2) .. 15])
